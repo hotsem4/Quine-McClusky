7f99683e747963e9198f0b3b517774c3bb124794
hotsem4 <hotsem100@gmail.com>
2023-05-23 13:25:21 +0900

    배열에 INPUT입력1

--- Quine-McClusky/소스.cpp
+++ Quine-McClusky/소스.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include <string.h>
 
+using namespace std;
+
 class Qm {
 public:
-	int firstArr[] = {}
+	int inputArr[16] = { 0, };  // 초기 input값 입력 배열
+
+	void inputSettingArr();
+	void init();
 };
+void Qm::inputSettingArr() {
+	int num = 0;
+	for (int i = 0; i < 16; i++) {
+		cout << i + 1 << "번째 입력 : ";
+		cin >> num;
+		inputArr[i] = num;
+	}
+}
+void Qm::init() {
+	inputSettingArr();
+	for (int i = 0; i < 16; i++) {
+		cout << inputArr[i] << " ";
+	}
+}
 
 int main() {
-	
+	Qm nm;
+	nm.init();
 }
