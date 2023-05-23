6896645bd740dcb61a563ff2a227481d2f0dd961
hotsem4 <hotsem100@gmail.com>
2023-05-23 15:39:51 +0900

    2차원 벡터 생성

--- Quine-McClusky/소스.cpp
+++ Quine-McClusky/소스.cpp
@@ -8,8 +8,9 @@ class Qm {
 public:
 	int inputArr[16] = { 0, };  // 초기 input값 입력 배열
 	vector<int> v;
-
+	vector<vector <int>> v2;
 	void inputSettingArr();
+	void makeDoubleVector();
 	void init();
 };
 
@@ -27,12 +28,34 @@ void Qm::inputSettingArr() {
 }
 
 // 벡터에 있는 10진수 값들을 하나씩 뽑아서 2진수로 변환하는 과정을 거쳐 2차원 벡터에 넣어준다.
+void Qm::makeDoubleVector() {
+	for (int i = 0; i < v.size(); i++) {
+		vector<int> binary;
+		int n = v[i];
+		for (int i = 0; i < 4; i++){
+			binary.insert(binary.begin(), n % 2);
+			n /= 2;
+		}
+		for (int j = 0; j < binary.size(); j++) {
+			cout << binary[j] << " ";
+		}
+		cout << "\n";
+		v2.push_back(binary);
+	}
+
+	// v2 print
+	cout << "\n";
+	for (int i = 0; i < v2.size(); i++) {
+		for (int j = 0; j < v2[i].size(); j++) {
+			cout << v2[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
 
 void Qm::init() {
 	inputSettingArr();
-	for (int i = 0; i < v.size(); i++) {
-		cout << v[i] << " ";
-	}
+	makeDoubleVector();
 }
 
 int main() {
