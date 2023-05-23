22db6b8848ba7c11c8867b8b3fb905a13d021bd6
hotsem4 <hotsem100@gmail.com>
2023-05-24 01:48:44 +0900

    v3를 만듬. v3는 0번째는 10진수 1번째는 2진수로 변환했을 때 1의 갯수이다.

--- Quine-McClusky/소스.cpp
+++ Quine-McClusky/소스.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string.h>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,9 +10,12 @@ public:
 	int inputArr[16] = { 0, };  // 초기 input값 입력 배열
 	vector<int> v;
 	vector<vector <int>> v2;
+	vector<vector <int>> v3;   // 10진수와 1의 갯수를 포함한 배열
 	void inputSettingArr();
 	void makeDoubleVector();
 	void printPage1();
+	void copyVector();
+	void printV3();
 	void init();
 };
 
@@ -42,7 +46,7 @@ void Qm::makeDoubleVector() {       // 벡터에 있는 10진수 값들을 하나씩 뽑아서 2
 
 void Qm::printPage1() {    // 2차원 벡터가 완성되었으니 1페이지 출력부 만들기
 	cout << "    " << "   A  " << "B" << "  C" << "  D  " << endl;
-	cout << "--------------------------------------------------" << endl;
+	cout << "-------------------" << endl;
 	for (int i = 0; i < v.size(); i++) {
 		if (v[i] < 10) {
 			cout << " " << v[i] << " " << " |  " << v2[i][0] << "  " << v2[i][1] << "  " << v2[i][2] << "  " << v2[i][3] << endl;
@@ -52,14 +56,40 @@ void Qm::printPage1() {    // 2차원 벡터가 완성되었으니 1페이지 출력부 만들기
 		}
 	}
 }
+int countOnes(const vector<int>& v) { return count(v.begin(), v.end(), 1); }  // 배열 안에 1이 몇 개 있는지 카운팅 함수  --> 매개변수 단일 벡서 넣으면 1의 갯수로 반환
+
+// 우선 v3에 v2를 복사한다.
+void Qm::copyVector() {
+	v3 = v2;
+	for (int i = 0; i < v.size(); i++) {
+		v3[i].insert(v3[i].begin(), v[i]);
+	}
+	int k = 0;
+	for (const auto & row : v2) {
+		int ones = countOnes(row);
+		v3[k].insert(v3[k].begin() + 1, ones);
+		k++;
+		
+	}
+}
+
+void Qm::printV3() {
+	for (int i = 0; i < v3.size(); i++) {
+		for (int j = 0; j < v3[i].size(); j++) {
+			cout << v3[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
 
 // 1의 원소의 갯수 별로 grouping 작업을 실시해야 한다.
 
-
 void Qm::init() {
 	inputSettingArr();
-	makeDoubleVector();
+	makeDoubleVector();	
 	printPage1();
+	copyVector();
+	printV3();
 }
 
 int main() {
@@ -71,4 +101,5 @@ int main() {
 /*
 v[i]는 v2[i][]의 10진수
 v2[i][]는 v[i]의 2진수
+v3는 0번째는 10진수가 있고 1번째는 2진수의 1의 갯수이다.
 */
